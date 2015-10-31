578aaa6a3182ca70e3ba05e117b209474aae05df
Peeter Ploom <pploom@klass2-07-l.intra.itcollege.ee>
2015-10-31 15:41:55 +0200

    uus laevade pommitamine

---
+++ src/teema2/Harjutus1_lipud.java
@@ -0,0 +1,11 @@
+package teema2;
+
+/**
+ * 1. Joonista eesti lipp kasutades kolme Rectangle
+ * 2. Joonista Jaapani lipp kasutades Rectangle ja Circle
+ */
+public class Harjutus1_lipud {
+    public static void main(String[] args) {
+
+    }
+}
