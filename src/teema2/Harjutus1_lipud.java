710d59e3e93c3f0c7349aa32d4b2fd96ada088bc
Peeter Ploom <pploom@klass2-07-l.intra.itcollege.ee>
2015-10-31 17:32:48 +0200

    uus

--- src/teema2/Harjutus1_lipud.java
+++ src/teema2/Harjutus1_lipud.java
@@ -1,11 +1,57 @@
 package teema2;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  * 1. Joonista eesti lipp kasutades kolme Rectangle
  * 2. Joonista Jaapani lipp kasutades Rectangle ja Circle
  */
-public class Harjutus1_lipud {
-    public static void main(String[] args) {
+public class Harjutus1_lipud extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane pane = new Pane();
+        Scene stseen = new Scene(pane);
+
+        Pane pane2 = new Pane();
+        Scene stseen2 = new Scene(pane2);
+
+        eestiLipp(pane);
+        jaapaniLipp(pane2);
+
+        Stage lava = new Stage();
+
+        lava.setScene(stseen2);
+        lava.show();
+
+        primaryStage.setScene(stseen);
+        primaryStage.show();
+    }
+
+    private void jaapaniLipp(Pane pane2) {
+        Rectangle kastj = new Rectangle(700, 400);
+        kastj.setFill(Color.WHITE);
+        Circle ring = new Circle(350, 200, 100);
+        ring.setFill(Color.RED);
+        pane2.getChildren().add(kastj);
+        pane2.getChildren().add(ring);
+
+    }
+
+    private void eestiLipp(Pane pane) {
+        Rectangle kast1 = new Rectangle(0, 0, 1100, 200);
+        Rectangle kast2 = new Rectangle(0, 200, 1100, 200);
+        Rectangle kast3 = new Rectangle(0, 400, 1100, 200);
+        kast1.setFill(Color.BLUE);
+        kast3.setFill(Color.WHITE);
+        pane.getChildren().add(kast1);
+        pane.getChildren().add(kast2);
+        pane.getChildren().add(kast3);
     }
 }
--- src/teema2/Harjutus3_logisisse.java
+++ src/teema2/Harjutus3_logisisse.java
@@ -11,11 +11,40 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Scanner;
+
 /**
  * 1. Loo sisse logimise ekraan (ainult visuaal)
  * 2. Määra üks võimalik kasutaja ja parool (andmebaasi veel ei kasuta)
  * 3. Ebaõnnestunud katse näitab kasutajale errorit
  * 4. Õnnestunud katse puhul vaheta pilt uue vastu (kasvõi roheline ring), .
  */
-public class Harjutus3_logisisse {
+public class Harjutus3_logisisse extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vbox = new VBox();
+        Scene stseen = new Scene(vbox);
+        primaryStage.setScene(stseen);
+        primaryStage.show();
+
+        Label kasutajaLabel = new Label("Kasutajanimi");
+        TextField kasutajainput = new TextField();
+        vbox.getChildren().addAll(kasutajaLabel, kasutajainput);
+
+        Label salasonaLabel = new Label("Salasõna");
+        PasswordField salasonainput = new PasswordField();
+        vbox.getChildren().addAll(salasonaLabel, salasonainput);
+
+        Button btn = new Button("Logi sisse");
+        btn.setOnAction(event -> {
+            String kasutajaSisu = kasutajainput.getText();
+            String kasutajaSisu = salasonainput.getText();
+
+
+
+        });
+        vbox.getChildren().addAll(btn);
+
+
+    }
 }
